2842fa93e98d62af64549038e3af55c659c7e949
niaoti <vincent@niaoti.com>
2022-12-07 14:22:37 +0100

    pytest unit test

---
+++ essai.py
@@ -0,0 +1,5 @@
+# i want to execute essai.py and print the result in console
+# i tried to use the command "python essai.py" but it doesn't work
+
+if(__name__ == "__main__"):
+    print("hello world")
--- notes/forms.py
+++ notes/forms.py
@@ -9,7 +9,7 @@ class NotesForm(forms.ModelForm):
         fields = ("title", "text")
         widgets = {
             'title' : forms.TextInput(attrs={'class':'form-control my-5'}),
-            'title' : forms.Textarea(attrs={'class':'form-control mb-5'})
+            'text' : forms.Textarea(attrs={'class':'form-control mb-5'})
         }
         labels = {
             'text' : 'label text'
@@ -17,7 +17,7 @@ class NotesForm(forms.ModelForm):
 
     def clean_title(self):
         title = self.cleaned_data['title']
-        if 'Django' not in title:
-            raise ValidationError('We accept note about Django')
+        # if 'Django' not in title:
+        #     raise ValidationError('We accept note about Django')
 
         return title
